79946eb7da8b4ccffada7e4416a4a5f8fc67c723
Hishan98 <hishansjc@gmail.com>
2020-04-23 07:14:30 +0530

    update

--- Expert/index.php
+++ Expert/index.php
@@ -222,8 +222,7 @@ if (!isset($_SESSION['user_type'])) {
                         <div class="row">
                             <div class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
                                 data-animate-effect="fadeInLeft">
-                                <span class="heading-meta">What I do?</span>
-                                <h2 class="colorlib-heading">Here are some of my expertise</h2>
+                                <h2 class="colorlib-heading">Student list</h2>
                             </div>
                         </div>
                         <div class="row row-pt-md">
